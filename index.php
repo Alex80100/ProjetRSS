249d0f0be2a32097bdb9ad75a6426a521586afbc
Alex80100 <flamentalexandre20@gmail.com>
2023-04-27 12:07:49 +0200

    fusion fichier card + index

--- index.php
+++ index.php
@@ -1,23 +1,82 @@
-<?php
-include __DIR__ . '/herlpers/dd.php';
+<!DOCTYPE html>
+<html lang="fr">
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/public/assets/css/style.css">
+    <title>projetRSS</title>
+</head>
 
-$url = 'https://rmcsport.bfmtv.com/rss/football/';
-$xml = simplexml_load_file($url);
-// var_dump($xml->channel->item[0]->title);
-d($xml->channel->item[0]);
-$items = $xml->channel->item;
-// echo $xml->channel->title;  //titre
-// echo $xml->channel->lastBuildDate; //date 
-?>
-<!-- <?php
-foreach ($items as $key => $item) { ?>
-    <div class="cardContainer">
-        <h1><?= $item->title ?></h1>
+<body>
+    <!------------------------- header------------------------------ -->
+    <!-------------------------LOGO------------------------------ -->
+    <img class="logo" src="public/assets/img/LOGO projetRSS.png" alt="Logo">
+    <!------------------------- Navbar------------------------------ -->
+
+    <div class="containernav">
+
+        <nav class="navbar">
+            <ul>
+                <li><a href="">Foot</a></li>
+                <li><a href="">Rugby</a></li>
+                <li><a href="">Basket</a></li>
+                <li><a href="">Tennis</a></li>
+                <li><a href="">Formule 1</a></li>
+            </ul>
+        </nav>
     </div>
-<?php } ?>
+    <!------------------------- Fin de Navbar------------------------------ -->
+    <!-------------------------Formulaire------------------------------ -->
+
+    <form>
+        <h1>Vos préferences:</h1>
+        <!-- <div class="containerform"> -->
+        <div class="containerform">
+            <div class="formulaire">
+                <!-- <div> -->
+                <input type="checkbox" id="sport" name="sport">
+                <label for="sport">Foot</label>
+                <!-- </div> -->
+
+                <!-- <div> -->
+                <input type="checkbox" id="sport" name="sport">
+                <label for="horns">Rugby</label>
+                <!-- </div> -->
+                <!-- <div> -->
+                <input type="checkbox" id="sport" name="sport">
+                <label for="sport">Basket</label>
+                <!-- </div> -->
+                <!-- <div> -->
+                <input type="checkbox" id="sport" name="sport">
+                <label for="sport">Tennis</label>
+                <!-- </div> -->
+                <!-- <div> -->
+                <input type="checkbox" id="sport" name="sport">
+                <label for="sport">Formule1</label>
+                <!-- </div> -->
+                <div class="dark">
+                    <legend>Dark mode:</legend>
 
 
-<div class="cardContainer">
-    <h1>titre 2</h1>
-</div> -->
+                    <input type="radio" id="dark" name="dark" value="dark">
+                    <label for="dark">Oui</label>
+
+
+                    <input type="radio" id="dark" name="dark" value="dark">
+                    <label for="dark">Non</label>
+    </form>
+    </div>
+    </div>
+    </div>
+    <!------------------------- Fin de formulaire------------------------------ -->
+    <!------------------------- Footer------------------------------ -->
+    <div class="footer">
+
+        <img class="logofooter" src="public/assets/img/LOGO projetRSS.png" alt="">
+        <h2>C'EST LA PASSION QUI PARLE</h2>
+    </div>
