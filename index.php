e6ced5244754a4277a5861e7b36ac7062dc2acf1
Alex80100 <flamentalexandre20@gmail.com>
2023-04-27 11:51:15 +0200

    push test

--- index.php
+++ index.php
@@ -2,84 +2,84 @@
 <html lang="fr">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=, initial-scale=1.0">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="/public/assets/css/style.css">
-  <title>projetRSS</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/public/assets/css/style.css">
+    <title>projetRSS</title>
 </head>
 
 <body>
-  <!------------------------- header------------------------------ -->
-  <!-------------------------LOGO------------------------------ -->
-  <img class="logo" src="public/assets/img/LOGO projetRSS.png" alt="Logo">
-  <!------------------------- Navbar------------------------------ -->
+    <!------------------------- header------------------------------ -->
+    <!-------------------------LOGO------------------------------ -->
+    <img class="logo" src="public/assets/img/LOGO projetRSS.png" alt="Logo">
+    <!------------------------- Navbar------------------------------ -->
 
-  <div class="containernav">
+    <div class="containernav">
 
-    <nav class="navbar">
-      <ul>
-        <li><a href="">Foot</a></li>
-        <li><a href="">Rugby</a></li>
-        <li><a href="">Basket</a></li>
-        <li><a href="">Tennis</a></li>
-        <li><a href="">Formule 1</a></li>
-      </ul>
-    </nav>
-  </div>
-  <!------------------------- Fin de Navbar------------------------------ -->
-  <!-------------------------Formulaire------------------------------ -->
+        <nav class="navbar">
+            <ul>
+                <li><a href="">Foot</a></li>
+                <li><a href="">Rugby</a></li>
+                <li><a href="">Basket</a></li>
+                <li><a href="">Tennis</a></li>
+                <li><a href="">Formule 1</a></li>
+            </ul>
+        </nav>
+    </div>
+    <!------------------------- Fin de Navbar------------------------------ -->
+    <!-------------------------Formulaire------------------------------ -->
 
-  <form>
-    <h1>Vos préferences:</h1>
-    <!-- <div class="containerform"> -->
-    <div class="containerform">
-      <div class="formulaire">
-        <!-- <div> -->
-        <input type="checkbox" id="sport" name="sport">
-        <label for="sport">Foot</label>
-        <!-- </div> -->
+    <form>
+        <h1>Vos préferences:</h1>
+        <!-- <div class="containerform"> -->
+        <div class="containerform">
+            <div class="formulaire">
+                <!-- <div> -->
+                <input type="checkbox" id="sport" name="sport">
+                <label for="sport">Foot</label>
+                <!-- </div> -->
 
-        <!-- <div> -->
-        <input type="checkbox" id="sport" name="sport">
-        <label for="horns">Rugby</label>
-        <!-- </div> -->
-        <!-- <div> -->
-        <input type="checkbox" id="sport" name="sport">
-        <label for="sport">Basket</label>
-        <!-- </div> -->
-        <!-- <div> -->
-        <input type="checkbox" id="sport" name="sport">
-        <label for="sport">Tennis</label>
-        <!-- </div> -->
-        <!-- <div> -->
-        <input type="checkbox" id="sport" name="sport">
-        <label for="sport">Formule1</label>
-        <!-- </div> -->
-        <div class="dark">
-          <legend>Dark mode:</legend>
-
-
-          <input type="radio" id="dark" name="dark" value="dark">
-          <label for="dark">Oui</label>
+                <!-- <div> -->
+                <input type="checkbox" id="sport" name="sport">
+                <label for="horns">Rugby</label>
+                <!-- </div> -->
+                <!-- <div> -->
+                <input type="checkbox" id="sport" name="sport">
+                <label for="sport">Basket</label>
+                <!-- </div> -->
+                <!-- <div> -->
+                <input type="checkbox" id="sport" name="sport">
+                <label for="sport">Tennis</label>
+                <!-- </div> -->
+                <!-- <div> -->
+                <input type="checkbox" id="sport" name="sport">
+                <label for="sport">Formule1</label>
+                <!-- </div> -->
+                <div class="dark">
+                    <legend>Dark mode:</legend>
 
 
-          <input type="radio" id="dark" name="dark" value="dark">
-          <label for="dark">Non</label>
-  </form>
-  </div>
-  </div>
-  </div>
-  <!------------------------- Fin de formulaire------------------------------ -->
-  <!------------------------- Footer------------------------------ -->
-  <div class="footer">
+                    <input type="radio" id="dark" name="dark" value="dark">
+                    <label for="dark">Oui</label>
 
-    <img class="logofooter" src="public/assets/img/LOGO projetRSS.png" alt="">
-    <h2>C'EST LA PASSION QUI PARLE</h2>
-  </div>
+
+                    <input type="radio" id="dark" name="dark" value="dark">
+                    <label for="dark">Non</label>
+    </form>
+    </div>
+    </div>
+    </div>
+    <!------------------------- Fin de formulaire------------------------------ -->
+    <!------------------------- Footer------------------------------ -->
+    <div class="footer">
+
+        <img class="logofooter" src="public/assets/img/LOGO projetRSS.png" alt="">
+        <h2>C'EST LA PASSION QUI PARLE</h2>
+    </div>
 
 
 
