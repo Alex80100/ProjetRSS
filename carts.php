c46dfcdc17e1842e5b2f269a3f78e72eebc96d97
Alex80100 <flamentalexandre20@gmail.com>
2023-04-27 16:20:52 +0200

    Integration rss 1 sans css

--- carts.php
+++ carts.php
@@ -1,3 +1,12 @@
+<?php
+include __DIR__ . '/herlpers/dd.php';
+$url = 'https://rmcsport.bfmtv.com/rss/football/';
+$xml = simplexml_load_file($url);
+$items = $xml->channel->item->enclosure['url'];
+$img = $xml->channel->item->enclosure['url'];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,24 +19,26 @@
 
 <body>
     <div class="container">
-        <div class="carts">
-            <div class="title">
-                <h3></h3>
+        <!-- Generation des cartes avec boucles foreach -->
+        <?php foreach ($items as $key => $item) { ?>
+            <div class="carts">
+                <div class="title">
+                    <h3><?= $item->title ?></h3>
+                </div>
+                <div class="date">
+                    <p><?= $item->pubDate ?></p>
+                </div>
                 <div class="image">
                     <img src="" alt="">
-                    <div class="description">
-                        <p></p>
-                        <div class="btn">
-                        <input type="submit" value="Valider">
-
-
-                        </div>
-                    </div>
+                </div>
+                <div class="description">
+                    <p></p>
+                </div>
+                <div class="btn">
+                    <input type="submit" value="Valider">
                 </div>
             </div>
-
-        </div>
-
+        <?php } ?>
     </div>
 
 </body>
