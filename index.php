a7d0421eb81b6e7a322a1d2af423e6d61fd73e35
Alex80100 <flamentalexandre20@gmail.com>
2023-04-27 11:35:24 +0200

    Commit test card php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+include __DIR__ . '/herlpers/dd.php';
+
+
+$url = 'https://rmcsport.bfmtv.com/rss/football/';
+$xml = simplexml_load_file($url);
+// var_dump($xml->channel->item[0]->title);
+d($xml->channel->item[0]);
+$items = $xml->channel->item;
+echo $xml->channel->title;  //titre
+echo $xml->channel->lastBuildDate; //date 
+echo $xml->channel->image->link;
+?>
+<?php
+foreach ($items as $key => $item) { ?>
+    <div class="cardContainer">
+        <h1><?= $item->title ?></h1>
+    </div>
+<?php } ?>
+
+
+<div class="cardContainer">
+    <h1>titre 2</h1>
+</div>
