8dfc06fb3e9a2b454073902f2e0b3cec2e6f56a7
Alex80100 <flamentalexandre20@gmail.com>
2023-04-27 11:47:16 +0200

    commit 2

--- index.php
+++ index.php
@@ -7,11 +7,10 @@ $xml = simplexml_load_file($url);
 // var_dump($xml->channel->item[0]->title);
 d($xml->channel->item[0]);
 $items = $xml->channel->item;
-echo $xml->channel->title;  //titre
-echo $xml->channel->lastBuildDate; //date 
-echo $xml->channel->image->link;
+// echo $xml->channel->title;  //titre
+// echo $xml->channel->lastBuildDate; //date 
 ?>
-<?php
+<!-- <?php
 foreach ($items as $key => $item) { ?>
     <div class="cardContainer">
         <h1><?= $item->title ?></h1>
@@ -21,4 +20,4 @@ foreach ($items as $key => $item) { ?>
 
 <div class="cardContainer">
     <h1>titre 2</h1>
-</div>
+</div> -->
