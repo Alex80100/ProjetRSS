d874cdcb70d9bb6ded7ac090815e0d52bbf7b87b
Alex80100 <flamentalexandre20@gmail.com>
2023-04-28 10:55:00 +0200

    condition url et supression pages actusBasket...

--- controlers/pages-controller.php
+++ controlers/pages-controller.php
@@ -1,7 +1,20 @@
 <?php 
 
 include __DIR__ . '/../helpers/dd.php';
-$url = 'https://rmcsport.bfmtv.com/rss/football/';
+
+if ($_GET['url'] == 1 ) {
+    $url = 'https://rmcsport.bfmtv.com/rss/football/';
+} elseif ($_GET['url'] == 2 ) {
+    $url = 'https://rmcsport.bfmtv.com/rss/rugby/';
+} elseif ($_GET['url'] == 3 ) {
+    $url = 'https://rmcsport.bfmtv.com/rss/basket/';
+} elseif ($_GET['url'] == 4 ) {
+    $url = 'https://rmcsport.bfmtv.com/rss/tennis/';
+}  elseif ($_GET['url'] == 5 ) {
+    $url = 'https://rmcsport.bfmtv.com/rss/auto-moto/f1/';
+}
+
+
 $xml = simplexml_load_file($url);
 $items = $xml->channel->item;
 $link = $items->link;
